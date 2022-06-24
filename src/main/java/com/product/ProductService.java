2d3c19ceef50047e32e7e6b2c46a1e5074e0b370
SosaTheHustler <arcane1241@gmail.com>
2022-06-24 16:14:51 -0400

    products connect to DB and API

--- src/main/java/com/product/ProductService.java
+++ src/main/java/com/product/ProductService.java
@@ -1,9 +1,15 @@
 package com.product;
 
+import com.common.utils.ResourceCreationResponse;
+import com.product.dtos.NewProductRequest;
+import com.product.dtos.ProductResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -12,6 +18,30 @@ public class ProductService {
 
     @Autowired
     public ProductService(ProductRepository productRepository) {
+
         this.productRepository = productRepository;
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(ProductResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public ProductResponse getUserById(int id) throws Exception {
+        return productRepository.findById(id).map(ProductResponse::new).orElseThrow(Exception::new);
+    }
+
+    public ResourceCreationResponse createProduct(@Valid NewProductRequest newProductRequest) {
+        Products newProduct = newProductRequest.extractResource();
+
+        productRepository.save(newProduct);
+        return new ResourceCreationResponse(newProduct.getId());
+    }
+
+    public void deleteProductById(int id) {
+        productRepository.deleteById(id);
+
     }
 }
